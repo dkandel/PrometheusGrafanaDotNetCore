44679d7fd7fab8600542c99de591673fc36111a0
Diwas <diwaskandel19@gmail.com>
2024-08-18 15:19:45 +1000

    add intitial project

---
+++ PrometheusGrafanaDotNetCore/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrometheusGrafanaDotNetCore.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
